d11c53f00dbaf14276a985c6df58c7be54805792
saikat1016 <saikat1016@gmail.com>
2019-07-25 15:21:52 +0600

    Initial Commit

---
+++ IosVisibleGone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IosVisibleGone
+//
+//  Created by Flora PC on 25/7/19.
+//  Copyright © 2019 Green Appers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
